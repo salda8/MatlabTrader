9baa25ea3de896593d23f3cf4b4702e01a98578f
vsalovsk <vojtech.salovsky-external@gemalto.com>
2017-01-04 13:10:09 +0100

    Installed expressmapper.

---
+++ MappingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATLAB_trader
+{
+    public class MappingConfiguration
+    {
+        Mapper.Register<>
+    }
+}
